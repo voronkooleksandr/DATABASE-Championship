cb55f76f877cb39332d55c616c1d23538c231f45
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-19 15:13:49 +0200

    Update create-table-Stadiums.sql

--- create-table-Stadiums.sql
+++ create-table-Stadiums.sql
@@ -2,9 +2,9 @@ USE Championship;
 GO
 
 CREATE TABLE Stadiums (
-	id INT IDENTITY NOT NULL,
+	id INT IDENTITY,
 	[name] VARCHAR(45) NOT NULL,
-  city VARCHAR(45) NOT NULL,
-  capacity INT NOT NULL,
+        city VARCHAR(45) NOT NULL,
+        capacity INT NOT NULL,
 	CONSTRAINT PK_Stadiums_id PRIMARY KEY (id)
 );
