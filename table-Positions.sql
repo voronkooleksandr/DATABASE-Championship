47fa21ce9c8700c4c861f7f3053da7d78e212461
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-19 20:07:33 +0200

    Update and rename create-table-Positions.sql to table-Positions.sql

--- table-Positions.sql
+++ table-Positions.sql
@@ -6,3 +6,11 @@ CREATE TABLE Positions (
 	[name] VARCHAR(45) NOT NULL,
 	CONSTRAINT PK_Positions_id PRIMARY KEY (id)
 );
+
+INSERT INTO Positions ([name])
+VALUES 
+('воротар'),
+('захисник'),
+('півзахисник'),
+('нападник');
+
