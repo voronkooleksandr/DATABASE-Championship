e3fa59fd21e0b5fe583ce9461840ffe1cc5f98ba
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-19 15:10:22 +0200

    Update create-table-Coaches.sql

--- create-table-Coaches.sql
+++ create-table-Coaches.sql
@@ -2,10 +2,11 @@ USE Championship;
 GO
 
 CREATE TABLE Coaches (
-	id INT IDENTITY NOT NULL,
-	[name] VARCHAR(45) NOT NULL,
-  surname VARCHAR(45) NOT NULL,
-	birthday DATE NOT NULL,
-  country VARCHAR(45) NOT NULL,
-	CONSTRAINT PK_Coaches_id PRIMARY KEY (id)
+    id INT IDENTITY,
+    [name] VARCHAR(45) NOT NULL,
+    surname VARCHAR(45) NOT NULL,
+    birthday DATE NOT NULL,
+    country VARCHAR(45) NOT NULL,
+    CONSTRAINT PK_Coaches_id PRIMARY KEY (id)
+    CONSTRAINT PK_Coaches_id PRIMARY KEY (id)
 );
