c8d36e81fdc24f58af5e9d3eeae8401a2a133bd9
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-19 17:30:03 +0200

    Update create-table-Games.sql

--- create-table-Games.sql
+++ create-table-Games.sql
@@ -8,7 +8,5 @@ CREATE TABLE Games (
   points_team_1 INT NOT NULL,
   points_team_2 INT NOT NULL,
   data_play DATE NOT NULL,
-  id_team_1 INT CONSTRAINT UQ_Games_id_team_1 UNIQUE,
-  id_team_2 INT CONSTRAINT UQ_Games_id_team_2 UNIQUE,
   CONSTRAINT PK_Games_id PRIMARY KEY (id)
 );
