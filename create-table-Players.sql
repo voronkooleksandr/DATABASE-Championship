8a2e57bffcf32ac4d93e75524f2eef978a5bb458
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-19 13:19:46 +0200

    Update create-table-Players.sql

--- create-table-Players.sql
+++ create-table-Players.sql
@@ -2,10 +2,11 @@ USE Championship;
 GO
 
 CREATE TABLE Players (
-	id INT IDENTITY PRIMARY KEY NOT NULL,
+	id INT IDENTITY NOT NULL,
 	[name] VARCHAR(45) NOT NULL,
 	surname VARCHAR(45) NOT NULL,
 	birthday DATE NOT NULL,
 	country VARCHAR(45) NOT NULL,
-	cost INT NOT NULL
+	cost INT NOT NULL,
+	CONSTRAINT PK_Players_id PRIMARY KEY (id)
 );
