9e076e1419d2e73639bf448d8697da6063d5d493
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-19 15:08:05 +0200

    Create create-table-Games.sql

---
+++ create-table-Games.sql
@@ -0,0 +1,14 @@
+USE Championship;
+GO
+
+CREATE TABLE Games (
+	id INT IDENTITY,
+  score_team_1 INT NOT NULL,
+  score_team_2 INT NOT NULL,
+  points_team_1 INT NOT NULL,
+  points_team_2 INT NOT NULL,
+  data_play DATE NOT NULL,
+  id_team_1 INT CONSTRAINT UQ_Games_id_team_1 UNIQUE,
+  id_team_2 INT CONSTRAINT UQ_Games_id_team_2 UNIQUE,
+	CONSTRAINT PK_Games_id PRIMARY KEY (id)
+);
