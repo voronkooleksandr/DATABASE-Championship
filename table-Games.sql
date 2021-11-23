ded1fc7666a93e8c669096e799efbccc6d8018af
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-23 14:19:42 +0200

    Update table-Games.sql

--- table-Games.sql
+++ table-Games.sql
@@ -2,21 +2,27 @@ USE Championship;
 GO
 
 CREATE TABLE Games (
-  id INT IDENTITY(1, 1),
-  score_team_1 INT NOT NULL,
-  score_team_2 INT NOT NULL,
-  points_team_1 INT NOT NULL,
-  points_team_2 INT NOT NULL,
-  data_play DATE NOT NULL,
+  id INT IDENTITY(1, 1)
   CONSTRAINT PK_Games_id PRIMARY KEY (id),
   id_team_1 INT NOT NULL
   CONSTRAINT UQ_Games_id_team_1 
   UNIQUE,
   CONSTRAINT FK_Games_id_team_1
   FOREIGN KEY (id_team_1)
-  REFERENCES Teams (id);
+  REFERENCES Teams (id),
+  id_team_2 INT NOT NULL
+  CONSTRAINT UQ_Games_id_team_2 
+  UNIQUE,
+  CONSTRAINT FK_Games_id_team_2
+  FOREIGN KEY (id_team_2)
+  REFERENCES Teams (id),
+  score_team_1 INT NOT NULL,
+  score_team_2 INT NOT NULL,
+  points_team_1 INT NOT NULL,
+  points_team_2 INT NOT NULL,
+  data_play DATE NOT NULL
 );
 
-INSERT INTO Games (score_team_1, score_team_2, points_team_1, points_team_2, data_play
+INSERT INTO Games (id_team_1, id_team_2, score_team_1, score_team_2, points_team_1, points_team_2, data_play)
 VALUES
 ()
