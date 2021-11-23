7fd0f02b670edf1438f629af8270b74c2e204c86
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-23 14:03:45 +0200

    Update table-Games.sql

--- table-Games.sql
+++ table-Games.sql
@@ -8,7 +8,13 @@ CREATE TABLE Games (
   points_team_1 INT NOT NULL,
   points_team_2 INT NOT NULL,
   data_play DATE NOT NULL,
-  CONSTRAINT PK_Games_id PRIMARY KEY (id)
+  CONSTRAINT PK_Games_id PRIMARY KEY (id),
+  id_team_1 INT NOT NULL
+  CONSTRAINT UQ_Games_id_team_1 
+  UNIQUE,
+  CONSTRAINT FK_Games_id_team_1
+  FOREIGN KEY (id_team_1)
+  REFERENCES Teams (id);
 );
 
 INSERT INTO Games (score_team_1, score_team_2, points_team_1, points_team_2, data_play
