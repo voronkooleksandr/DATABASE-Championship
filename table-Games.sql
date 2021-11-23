1f356d6dd5e5141ac631e48a094b4923aeb8e5b0
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-23 13:57:45 +0200

    Update and rename create-table-Games.sql to table-Games.sql

--- table-Games.sql
+++ table-Games.sql
@@ -2,7 +2,7 @@ USE Championship;
 GO
 
 CREATE TABLE Games (
-  id INT IDENTITY,
+  id INT IDENTITY(1, 1),
   score_team_1 INT NOT NULL,
   score_team_2 INT NOT NULL,
   points_team_1 INT NOT NULL,
@@ -10,3 +10,7 @@ CREATE TABLE Games (
   data_play DATE NOT NULL,
   CONSTRAINT PK_Games_id PRIMARY KEY (id)
 );
+
+INSERT INTO Games (score_team_1, score_team_2, points_team_1, points_team_2, data_play
+VALUES
+()
