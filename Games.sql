7260c987f374dd7e15c5d7ba722632e9b0e23da3
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-29 17:11:44 +0200

    Update Games.sql

--- Games.sql
+++ Games.sql
@@ -16,9 +16,7 @@ CREATE TABLE Games (
   CONSTRAINT FK_Games_id_team_1 FOREIGN KEY (id_team_1) REFERENCES Teams (id),
   CONSTRAINT FK_Games_id_team_2 FOREIGN KEY (id_team_2) REFERENCES Teams (id),
   CONSTRAINT FK_Games_id_stadium FOREIGN KEY (id_stadium) REFERENCES Stadiums (id),
-  CONSTRAINT UQ_Games_id_team_1 UNIQUE (id_team_1),
-  CONSTRAINT UQ_Games_id_team_2 UNIQUE (id_team_2),
-  CONSTRAINT CK_Games_id_stadium CHECK (id_stadium = Games.id_team_1 )
+  CONSTRAINT CK_Games_id_stadium CHECK (id_stadium = Games.id_team_1)
   );
 
 INSERT INTO Games (id_team_1, id_team_2, score_team_1, score_team_2, points_team_1, points_team_2, data_play)
@@ -107,7 +105,7 @@ VALUES
 -- 10 тур
 (0, 2, 0, 3, '2021-10-01', 3, 12, 3),
 (2, 1, 3, 0, '2021-10-02', 15, 8, 15),
-(0, 1, 0, 3, '2021-10-02', 11, 6, 1),
+(0, 1, 0, 3, '2021-10-02', 11, 6, 11),
 (3, 0, 3, 0, '2021-10-02', 5, 7, 5),
 (0, 0, 1, 1, '2021-10-03', 14, 10, 14),
 (2, 0, 3, 0, '2021-10-03', 9, 13, 9),
