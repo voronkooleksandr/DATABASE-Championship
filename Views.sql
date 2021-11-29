c8cf1717b96f678728061f3380e4f488306389f0
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-29 11:09:53 +0200

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,13 +1,13 @@
 USE Championship;
 GO
 
--- Показ у віртуальній таблиці команд, які грають між собою, дату та місце проведення (стадіон)
+-- Показ у віртуальній таблиці команд, які грають між собою, дату гри та місце проведення (стадіон)
 
 CREATE VIEW GamesTeamsStadiums AS
 SELECT Games.team_1 AS Team_1,
        Games.team_2 AS Team_2,
        Stadiums.id_stadium_1 AS Stadium,
-       Games
+       Games.data_play AS GamesDate
 FROM Games JOIN Teams ON Games.team_1 = Teams.id
 JOIN Teams ON Games.team_2 = Teams.id
 JOIN Studiums ON Stadiums.id_stadium_1 = Stadiums.id;
