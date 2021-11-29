920bc5a04e6194882f0c012cb21e47cb41af23a1
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-29 11:41:20 +0200

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -12,20 +12,24 @@ FROM Games JOIN Teams ON Games.team_1 = Teams.id
 JOIN Teams ON Games.team_2 = Teams.id
 JOIN Studiums ON Stadiums.id_stadium_1 = Stadiums.id;
 
+-- Додання нового гравця до представлення, що автоматично додасть також цього гравця в нашу основну таблицю
+
 CREATE VIEW PlayerNew
 AS SELECT Players.[name] AS Name_Player,
           Players.surname AS Surname,
           Players.birthday AS Birthday,
           Players.country AS Country,
+          Players.cost AS Cost,
           Players.id_position AS Position,
           Players.id_team AS Team
 FROM Players;           
 
--- Додання нового гравця до представлення, що автоматично додасть також цього гравця в нашу основну таблицю
-
 INSERT INTO PlayerNew (Name_Player, Surname, Birthday, Country, Position, Team)
-VALUES ('Андрій', 'Ярмоленко', '1989-10-23', 'Україна', 3, 1);
+VALUES ('Андрій', 'Ярмоленко', '1989-10-23', 'Україна', 2000000, 3, 1);
+
+-- Збільшення з/п гравцю, якого щойно додали чи старому, який був в осн. таблиці, де шукаємо його по імені чи ID
 
 UPDATE PlayerNew
-SET 
+SET Cost = Cost + Cost * 0,20
+WHERE Surname = 'Ярмоленко';
        
