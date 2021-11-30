ab7e332dc35b6e5af6c39c12d24af53cdae4889f
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-11-30 22:09:30 +0200

    Update TRIGGER.sql

--- TRIGGER.sql
+++ TRIGGER.sql
@@ -1,3 +1,12 @@
 USE Championship;
 GO
 
+CREATE TRIGGER Players_UPDATE_cost
+ON Players
+AFTER UPDATE 
+AS UPDATE Players
+SET cost = cost + cost * 0.22
+WHERE id = (SELECT id FROM inserted);
+
+SELECT * FROM Players_UPDATE_cost;
+
