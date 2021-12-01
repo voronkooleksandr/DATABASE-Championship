b4773cf1ba302876f15fffe5af51a4c44b33a4ab
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-12-01 11:40:33 +0200

    Update INDEXES.sql

--- INDEXES.sql
+++ INDEXES.sql
@@ -6,7 +6,11 @@ GO
 CREATE INDEX INX_Players_surname
 ON Players (surname);
 
--- індекс пошуку пари команд, які грають між собою
+-- індекс пошуку пари команд, які грають між собою та дату гри. Для зменшення розміру ключа (3 bite) data_play виносимо в неключовий стовбець
 
-CREATE INDEX INX_Game_id_teams
-ON Gamesrs (id_team_1,  id_team_2);
+CREATE INDEX INX_Game_id_teams_date
+ON Games (id_team_1, id_team_2)
+INCLUDE (data_play);
+
+
+
