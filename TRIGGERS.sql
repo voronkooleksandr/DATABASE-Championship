5058c719fd05f7d03838b9649b59fa9f4bf8d451
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-12-01 17:20:07 +0200

    Update TRIGGERS.sql

--- TRIGGERS.sql
+++ TRIGGERS.sql
@@ -14,3 +14,32 @@ INSERT INTO Players ([name], surname, birthday, country, cost, id_position, id_t
 VALUES
 ('Сергій', 'Ребров', '1975-04-22', 'Україна', 19000000, 4, 1);
 
+USE Championship;
+GO
+
+DROP TABLE IF EXISTS Foreigner;
+
+CREATE TABLE NewTeams (
+id INT CONSTRAINT PK_NewTeams_id PRIMARY KEY IDENTITY(1, 1),
+new_team_id INT NOT NULL,
+[name] VARCHAR (30) NOT NULL,
+city VARCHAR (45) NOT NULL,
+date_connected DATE NOT NULL DEFAULT GETDATE()
+);
+
+CREATE TRIGGER NewTeams_INSERT
+ON Teams
+AFTER INSERT
+AS
+INSERT INTO NewTeams (new_team_id, [name], city)
+SELECT id, 'Нова команда ' + [name], city,  '' буде грати в нашому чемпіонаті'
+FROM inserted;
+
+INSERT INTO Players ([name], surname, birthday, country, cost, id_position, id_team)
+VALUES
+('Кріштіано', 'Роналду', '1985-04-29', 'Португалія', 75000000, 4, 2);
+
+SELECT * FROM Players;
+
+SELECT * FROM Foreigner;
+
