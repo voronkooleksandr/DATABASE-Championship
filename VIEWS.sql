51bcd770306c42bab987c14261250d070481e44d
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-12-01 12:30:29 +0200

    Update VIEWS.sql

--- VIEWS.sql
+++ VIEWS.sql
@@ -4,18 +4,7 @@ GO
 -- Показ у віртуальній таблиці команд, які грають між собою, дату гри та місце проведення (стадіон)
 
 CREATE VIEW GamesTeamsStadiums
-AS 
-SELECT Games.team_1 AS Team_1,
-       Games.team_2 AS Team_2,
-       Stadiums.id_stadium AS Stadium,
-       Games.data_play AS GameDate
-FROM Games JOIN Teams ON Games.team_1 = Teams (id)
-JOIN Teams ON Games.team_2 = Teams (id)
-JOIN Studiums ON Games.id_stadium = Stadiums (id);
-
-CREATE VIEW GamesTeamsStadiums
-AS 
-SELECT T1.name AS Team_1,
+AS SELECT T1.name AS Team_1,
        T2.name AS Team_2,
        S.name AS Stadium,
        G.data_play AS GameDate
@@ -27,21 +16,24 @@ JOIN Stadiums AS S ON G.id_stadium = S.id;
 -- Додання нового гравця до представлення, що автоматично додасть також цього гравця в нашу основну таблицю
 
 CREATE VIEW PlayerNew
-AS SELECT Players.[name] AS Name_Player,
-          Players.surname AS Surname,
-          Players.birthday AS Birthday,
-          Players.country AS Country,
-          Players.cost AS Cost,
-          Players.id_position AS Position,
-          Players.id_team AS Team
-FROM Players;           
+AS SELECT P.[name] AS Name_Player,
+          P.surname AS Surname,
+          P.birthday AS Birthday,
+          P.country AS Country,
+          P.cost AS Cost,
+          P.id_position AS Position,
+          P.id_team AS Team
+FROM Players AS P;          
 
 INSERT INTO PlayerNew (Name_Player, Surname, Birthday, Country, Cost, Position, Team)
-VALUES ('Андрій', 'Ярмоленко', '1989-10-23', 'Україна', 20000000, 3, 1);
+VALUES
+('Андрій', 'Ярмоленко', '1989-10-22', 'Україна', 20000000, 3, 1),
+('Андрій', 'Шевченко', '1978-09-29', 'Україна', 45000000, 4, 1);
 
 -- Збільшення з/п гравцю, якого щойно додали чи старому, який був в осн. таблиці, де шукаємо його по імені чи ID
 
 UPDATE PlayerNew
-SET Cost = Cost + Cost * 0,20
-WHERE Surname = 'Ярмоленко';
-       
+SET Cost = Cost + Cost * 0.22
+WHERE Surname = 'Шевченко';
+
+SELECT * FROM PlayerNew;
